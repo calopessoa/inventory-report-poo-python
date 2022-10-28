6f2b956b8969a2fdee32b0104b4cb97efd35b51b
Carlos <nashlasterby@gmail.com>
2022-10-28 15:09:53 -0300

    feat: req01, test_product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Borracha',
+        'Papelaria Solar',
+        '2021-07-04',
+        '2029-02-09',
+        'FR48',
+        'Ao abrigo de luz solar'
+    )
+    assert type(product.id) == int
+    assert product.id == 1
+    assert product.nome_do_produto == 'Borracha'
+    assert product.nome_da_empresa == 'Papelaria Solar'
+    assert product.data_de_fabricacao == '2021-07-04'
+    assert product.data_de_validade == '2029-02-09'
+    assert product.numero_de_serie == 'FR48'
+    assert product.instrucoes_de_armazenamento == 'Ao abrigo de luz solar'
