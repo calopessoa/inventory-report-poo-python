e158416a7a1f4bd6afbabb1f3645fb13e2f4df4e
Carlos <nashlasterby@gmail.com>
2022-10-29 14:13:38 -0300

    feat: req05 csv reader only

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,18 @@
+import csv
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    def file_reader(path):
+        if path.endswith(".csv"):
+            with open(path, "r") as file:
+                return list(csv.DictReader(file))
+
+    @staticmethod
+    def import_data(path, type):
+        data = Inventory.file_reader(path)
+        if type == "simples":
+            return SimpleReport.generate(data)
+        else:
+            return CompleteReport.generate(data)
