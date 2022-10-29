e500aa9d99840b2080688bb42b6e544b6ce84deb
Carlos <nashlasterby@gmail.com>
2022-10-29 15:44:58 -0300

    feat: req09 test_colored_report

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,22 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importer.csv_importer import CsvImporter
+
+
+colored_report = (
+    "\033[32mData de fabricação mais antiga:\033[0m"
+    + " \033[36m2020-09-06\033[0m\n"
+    "\033[32mData de validade mais próxima:\033[0m"
+    + " \033[36m2023-09-17\033[0m\n"
+    "\033[32mEmpresa com mais produtos:\033[0m"
+    + " \033[31mTarget Corporation\033[0m"
+)
+
+file_path = "inventory_report/data/inventory.csv"
+csv_file = CsvImporter.import_data(file_path)
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    simple_colored_report = ColoredReport(SimpleReport).generate(csv_file)
+    for item, row in enumerate(simple_colored_report):
+        assert row == colored_report[item]
